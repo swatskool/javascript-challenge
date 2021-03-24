9419c469c6a44951d34ae1cafa28b180e5a6966e
swatskool <75335665+swatskool@users.noreply.github.com>
2021-03-23 22:25:53 -0600

    update

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,10 +3,6 @@
 const tableData = data;
 const tbody =d3.select('tbody');
 
-function printName(name) {
-    console.log(name['datetime']);
-  }
-
 
 // creating functions and utilities
 // function to publish the data table
@@ -22,37 +18,9 @@ function UFO_table(subdata){
 }
 
 
-// function to create a filter list based on users choices
-var filters = {}
-function updateFilters(){
-  // var changedElements = (d3.select(this)).select('input');
-  var changedElements = (d3).select('.form-group');
-  console.log(changedElements);
-  var elementValue = changedElements.property('value');
-  var ID = changedElements.attr('id');
-  console.log(changedElements.value);
-  if (elementValue){
-    filters[ID]=elementValue;
-
-  }
-  else {
-    delete filters[ID];
-  }
-  filter_data();
-}
-
-
-// function to filter the data based on the list
-function filter_data(){
-  let localdata = tableData;
-  Object.entries(filters).forEach(([key, value])=>{
-    localdata=localdata.filter(row=> row[key] ===value);
-  }
-  );
-  UFO_table(localdata);
-}
 
 function myfilter(){
+  
   const inDate = d3.select('#datetime').property('value');
   const inCity = d3.select('#city').property('value');
   const inState = d3.select('#state').property('value');
@@ -65,88 +33,17 @@ function myfilter(){
   if (inState){localdata=localdata.filter(row => row.state=== inState);}
   if (inCountry){localdata=localdata.filter(row => row.country=== inCountry);}
   if (inShape) {localdata=localdata.filter(row => row.shape=== inShape);}
-  console.log(localdata.length)
-  if (localdata.length==0) {localdata=localdata.filter(row => console.log(row.city));}
-  console.log(localdata.length==0);
-  console.log(localdata);
-  // console.log(localdata.datetime)
+  // if (localdata.length==0) {localdata=localdata.filter(row => row.datetime==='---'));}
+
   UFO_table(localdata);
 }
 
 
 
-// d3.selectAll("#filter-btn").on('click', updateFilters);
 d3.selectAll("#filter-btn").on('click', myfilter);
-// d3.selectAll("#filter-btn").on('change', updateFilters);
+
 
 UFO_table(tableData);
 
 
 
-
-
-
-// tableData.forEach(printName);
-// // var tbody = d3.select("tbody");
-// var row = tbody.append('tr')
-// var cell = row.append("td")
-
-// // function filter_date(info){
-// //    return info.datetime = inDate
-
-// }
-// function UFO(subdata) {
-  
-//     var row = tbody.append('tr');
-//     Object.entries(UFO).forEach(function([key,value]){
-//         var cell = row.append("td");
-//         console.log(key, value);
-//         cell.text(value);
-//     })
-// }
-
-// data.forEach(function(UFO) {
-//     //   console.log(UFO);
-//       var row = tbody.append("tr");
-//       Object.entries(UFO).forEach(function([key, value]) {
-//         // console.log(key, value);
-//         var cell = row.append("td");
-//         cell.text(value);
-//   });
-// });
-
-
-
-
-// var inputdate = d3.select('#datetime');
-// inputdate.on('change', function(){
-//    inDate = d3.event.target.value;
-//   console.log(inDate);
-// }
-// )
-
-// button.on("click", function() {
-//        var output = tableData.filter(tableData => tableData.datetime == inDate)
-//        console.log(output)
-//        tbody.html('')
-       
-
-//         output.forEach(function(UFO) {
-//             //  how to clear table before appending new results?
-//             // how to create a general function which can be called by all modules
-             
-//             var row = tbody.append("tr");
-//               Object.entries(UFO).forEach(function([key, value]) {
-//                 // console.log(key, value);
-               
-//                 var cell = row.append("td");
-//                 cell.text(value);
-//           });
-//         });
-
-
-
-
-     
-//   });
-  
